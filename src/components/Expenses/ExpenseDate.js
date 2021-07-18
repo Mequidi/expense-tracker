dfede9a2664e59783bea7bb16caf5824a0ab04fe
Mequidi <nikhilsejwal234@gmail.com>
2021-07-18 17:42:28 +0530

    removed warnings

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -2,21 +2,7 @@ import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
     
-    var monthsInYear = new Array();
-    monthsInYear[0] = "January";
-    monthsInYear[1] = "February";
-    monthsInYear[2] = "March";
-    monthsInYear[3] = "April";
-    monthsInYear[4] = "May";
-    monthsInYear[5] = "June";
-    monthsInYear[6] = "July";
-    monthsInYear[7] = "August";
-    monthsInYear[8] = "September";
-    monthsInYear[9] = "October";
-    monthsInYear[10] = "November";
-    monthsInYear[11] = "December";
-    
-
+    var monthsInYear = ["January","February","March","April","May", "June", "August","September","October","November","December"]; 
     const date = props.date.getDate();
     const month = monthsInYear[props.date.getMonth()];
     const year = props.date.getFullYear();
